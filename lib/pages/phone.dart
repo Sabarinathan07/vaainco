163f5402ea2fbfa774d8e2ea020f2b97feb0ced1
Sabari <r.sabarinathan02@gmail.com>
2023-05-13 15:44:24 +0530

    ui fix

--- lib/pages/phone.dart
+++ lib/pages/phone.dart
@@ -22,14 +22,18 @@ class _MyPhoneState extends State<MyPhone> {
     return Scaffold(
       body: Container(
         color: vPrimaryColor,
-        child: ListView(
-          padding: const EdgeInsets.only(left: 25, right: 25, top: 50),
+        padding: const EdgeInsets.only(left: 25, right: 25, top: 70,bottom: 20),
+        child:Expanded(
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+
           children: [
-            Row(
+            const Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 Icon(Icons.grid_view, color: vPrimaryLightColor, size: 44),
-                const SizedBox(height: 5),
+                SizedBox(height: 5,width: 20,),
                 Text(
                   "Vaainco",
                   style: TextStyle(
@@ -41,108 +45,134 @@ class _MyPhoneState extends State<MyPhone> {
                 ),
               ],
             ),
-            const SizedBox(height: 20),
-            const Text(
-              "Shopping made seamless \nScan skip and go!",
-              style: TextStyle(fontSize: 20,color: Colors.white, fontWeight: FontWeight.bold,
-              ),
-              textAlign: TextAlign.left,
+            // const SizedBox(height: 20),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+               children:[
+                 Text(
+                   "Shopping made seamless \nScan skip and go!",
+                   style: TextStyle(fontSize: 25,color: Colors.white, fontWeight: FontWeight.bold,
+                   ),
+                   textAlign: TextAlign.start,
+                 ),
+               ],
+
             ),
-            const SizedBox(height: 60),
-            Container(
-              margin: const EdgeInsets.only(top: 60),
-              height: 55,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border:
-                Border.all(width: 1, color: Colors.grey),
-                borderRadius: BorderRadius.circular(25),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  SizedBox(
-                    width: 40,
-                    child: TextField(
-                      controller: countryController,
-                      keyboardType: TextInputType.number,
-                      decoration: const InputDecoration(
-                        border: InputBorder.none,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  Expanded(
-                    child: TextField(
-                      keyboardType: TextInputType.phone,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Enter phone number",
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: double.infinity,
-              height: 55,
-              decoration: BoxDecoration(
-                color: Colors.white10,
-                border:
-                Border.all(width: 1, color: Colors.grey),
-                borderRadius: BorderRadius.circular(25),
-              ),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
+
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 40),
+                  height: 55,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border:
+                    Border.all(width: 1, color: Colors.grey),
                     borderRadius: BorderRadius.circular(25),
                   ),
-                  backgroundColor: Colors.white,
-                ),
-                onPressed: () {
-                  Navigator.pushNamed(context, 'verify');
-                },
-                child: const Text(
-                  "Continue",
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: vPrimaryColor,
-                      fontFamily: 'Poppins'
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      SizedBox(
+                        width: 40,
+                        child: TextField(
+                          controller: countryController,
+                          keyboardType: TextInputType.number,
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+
+                          ),
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      const Expanded(
+                        child: TextField(
+                          keyboardType: TextInputType.phone,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Enter phone number",
+                          ),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 55,
+                  decoration: BoxDecoration(
+                    color: Colors.white10,
+                    border:
+                    Border.all(width: 1, color: Colors.grey),
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      backgroundColor: Colors.white,
+                    ),
+                    onPressed: () {
+                      Navigator.pushNamed(context, 'verify');
+                    },
+                    child: const Text(
+                      "Continue",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: vPrimaryColor,
+                          fontFamily: 'Poppins'
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Container(
-              margin: const EdgeInsets.only(bottom: 10),
-              alignment: Alignment.center,
-              child: Text(
-                "© 2023 Vaainco. All Rights Reserved",
+            // const SizedBox(height: 40),
+
+            const SizedBox(
+              height:70,
+            ),
+
+
+           const Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+
+              Text(
+                "By continuing, you agree to our \nTerms of Service & Privacy Policy",
                 style: TextStyle(
-                  color: Colors.white,
+                  color: vPrimaryLightColor,
                   fontSize: 16,
                   fontFamily: 'Poppins',
-                  fontWeight: FontWeight.w300,
+                  fontWeight: FontWeight.w500,
                 ),
               ),
-            ),
+            ],
+          ),
 
-          ],
 
-        ),
 
+        ],
       ),
-
-
+        ),
+      ),
     );
   }
 }
--- lib/pages/verify.dart
+++ lib/pages/verify.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/constant.dart';
 import 'package:pinput/pinput.dart';
 
 class MyVerify extends StatefulWidget {
@@ -14,24 +15,24 @@ class _MyVerifyState extends State<MyVerify> {
     final defaultPinTheme = PinTheme(
       width: 56,
       height: 56,
-      textStyle: TextStyle(
+      textStyle: const TextStyle(
           fontSize: 20,
           color: Color.fromRGBO(30, 60, 87, 1),
           fontWeight: FontWeight.w600),
       decoration: BoxDecoration(
-        border: Border.all(color: Color.fromRGBO(234, 239, 243, 1)),
+        border: Border.all(color: const Color.fromRGBO(234, 239, 243, 1)),
         borderRadius: BorderRadius.circular(20),
       ),
     );
 
     final focusedPinTheme = defaultPinTheme.copyDecorationWith(
-      border: Border.all(color: Color.fromRGBO(114, 178, 238, 1)),
+      border: Border.all(color: const Color.fromRGBO(114, 178, 238, 1)),
       borderRadius: BorderRadius.circular(8),
     );
 
     final submittedPinTheme = defaultPinTheme.copyWith(
       decoration: defaultPinTheme.decoration?.copyWith(
-        color: Color.fromRGBO(234, 239, 243, 1),
+        color: const Color.fromRGBO(234, 239, 243, 1),
       ),
     );
 
@@ -43,97 +44,155 @@ class _MyVerifyState extends State<MyVerify> {
           onPressed: () {
             Navigator.pop(context);
           },
-          icon: Icon(
+          icon: const Icon(
             Icons.arrow_back_ios_rounded,
-            color: Colors.black,
+            color: Colors.white,
           ),
         ),
         elevation: 0,
       ),
       body: Container(
-        margin: const EdgeInsets.only(left: 25, right: 25),
-        alignment: Alignment.topLeft,
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
+        color: vPrimaryColor,
+        padding: const EdgeInsets.only(left: 25, right: 25,top:50),
+        child : Expanded(
 
-              const Text(
-                "OTP",
-                style: TextStyle(fontSize: 42, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                "Verification",
-                style: TextStyle(fontSize: 42, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(
-                height: 25,
-              ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
 
-              Text(
-                "OTP has been sent to your number",
-                style: TextStyle(fontSize: 15),
-              ),
-              SizedBox(
-                height: 10,
-              ),
+                const Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children:[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
 
-              SizedBox(
-                height: 30,
-              ),
-              Pinput(
-                length: 4,
-                // defaultPinTheme: defaultPinTheme,
-                // focusedPinTheme: focusedPinTheme,
-                // submittedPinTheme: submittedPinTheme,
+                      children: [
+                        Text(
+                          "OTP \nVerification",
+                          style: TextStyle(
+                              fontSize: 45,
+                              fontWeight: FontWeight.w700,
+                              color: vPrimaryLightColor,
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "OTP has been sent to your number",
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                              color:Colors.white,
 
-                showCursor: true,
-                onCompleted: (pin) => print(pin),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              // SizedBox(
-              //   width: double.infinity,
-              //   height: 45,
-              //   child: ElevatedButton(
-              //       style: ElevatedButton.styleFrom(
-              //           primary: Colors.green.shade600,
-              //           shape: RoundedRectangleBorder(
-              //               borderRadius: BorderRadius.circular(10))),
-              //       onPressed: () {},
-              //       child: Text("Verify Phone Number")),
-              // ),
-              SizedBox(
-                child: Text(
-                  "00:29",
+                          ),
+                          textAlign: TextAlign.start,
+                        ),
+                      ],
+
+                    ),
+                  ]
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextButton(
-                      onPressed: () {
-                        Navigator.pushNamedAndRemoveUntil(
-                          context,
-                          'phone',
-                              (route) => false,
-                        );
-                      },
-                      child: Text(
-                        "Didn't get it?",
-                        style: TextStyle(color: Colors.black, ),
-                      ))
-                ],
-              ),
-              TextButton(onPressed: (){
-              //  resend otp function
-              }, child: Text(
-                "Send OTP (SMS)"
-              ))
-            ],
-          ),
+
+
+
+
+
+
+                const SizedBox(
+                  height: 10,
+                ),
+
+
+                Pinput(
+                  length: 4,
+                  defaultPinTheme: defaultPinTheme,
+                  // focusedPinTheme: focusedPinTheme,
+                  submittedPinTheme: submittedPinTheme,
+
+                  showCursor: true,
+
+                  onCompleted: (pin) => print(pin),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                // SizedBox(
+                //   width: double.infinity,
+                //   height: 45,
+                //   child: ElevatedButton(
+                //       style: ElevatedButton.styleFrom(
+                //           primary: Colors.green.shade600,
+                //           shape: RoundedRectangleBorder(
+                //               borderRadius: BorderRadius.circular(10))),
+                //       onPressed: () {},
+                //       child: Text("Verify Phone Number")),
+                // ),
+                const SizedBox(
+                  child: Text(
+                    "00:29",
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: vPrimaryLightColor,
+
+                    ),
+                    textAlign: TextAlign.justify,
+
+                  ),
+                ),
+
+                 Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton(
+                            onPressed: () {
+                              Navigator.pushNamedAndRemoveUntil(
+                                context,
+                                'phone',
+                                    (route) => false,
+                              );
+                            },
+                            child: const Text(
+                              "Didn't get it?",
+                              style: TextStyle(color: Colors.white, ),
+                            ))
+                      ],
+                    ),
+                    TextButton(onPressed: (){
+                      //  resend otp function
+                    }, child: const Text(
+                        "Send OTP (SMS)",
+                            style: TextStyle(
+                              color: Colors.white30,
+
+                            ),
+                    ),
+                    ),
+                    const SizedBox(
+                      height: 100,
+                    ),
+                  ],
+                ),
+
+              ],
+            ),
+
+//----
+
+
         ),
+
+        // alignment: Alignment.topLeft,
+
       ),
     );
   }
